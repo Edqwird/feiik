4c313af4c7906ee283c6f2b36e3080ce54e0f0cf
Edqwird <161136059+Edqwird@users.noreply.github.com>
2025-12-06 13:59:36 +0400

    Add files via upload

--- eji.js
+++ eji.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         
         if (document.querySelector('.sum')) {
-            document.querySelector('.sum').textContent = '-' + (data.sum || '0') + ' ₽';
+            document.querySelector('.sum').textContent = '-' + (data.sum || '0') + ' <span class="ruble">₽</span>';
         }
         
         if (document.querySelector('.sum-text')) {
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const sum = parseFloat(data.sum) || 0;
             const newBalance = balance - sum;
             document.querySelector('.sum-text').textContent = 
-                `${balance} ₽ ➜ ${newBalance} ₽`;
+                `${balance} <span class="ruble">₽</span> 🠒 ${newBalance} <span class="ruble">₽</span>`;
         }
         
         
